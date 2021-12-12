75a19274cf1e97abde3fd0baab95dacfe6b858b4
hemantnyadav <hemantnyadav@gmail.com>
2021-12-12 13:20:01 +0530

    TSF_jena.py is workigng with plots

--- LSTM_Jena/TSF_jena.py
+++ LSTM_Jena/TSF_jena.py
@@ -14,11 +14,30 @@ from WindowGenerator import WindowGenerator
 train_df = pd.read_csv('preprocessed_normalized_train_df_70.csv')
 val_df = pd.read_csv('preprocessed_normalized_validation_df_20.csv')
 test_df = pd.read_csv('preprocessed_normalized_test_df_10.csv')
+
+print("Data is read")
+
 column_indices = {name: i for i, name in enumerate(train_df.columns)}
 num_features = train_df.shape[1]
-
 val_performance = {}
 performance = {}
+
+#single_step_window
+single_step_window = WindowGenerator(
+    input_width=1, label_width=1, shift=1,
+    train_df=train_df, val_df=val_df, test_df=test_df,
+    label_columns=['T (degC)'])
+
+print("single_step_window ")
+
+# Wide Window Model
+wide_window = WindowGenerator(
+    input_width=24, label_width=24, shift=1,
+    train_df=train_df, val_df=val_df, test_df=test_df,
+    label_columns=['T (degC)'])
+
+print("wide_window ")
+
 
 MAX_EPOCHS = 20
 
@@ -36,6 +55,9 @@ def compile_and_fit(model, window, patience=2):
                       callbacks=[early_stopping], verbose=0)
   return history
 
+
+# Baseline Model
+
 class Baseline(tf.keras.Model):
     def __init__(self, label_index=None):
         super().__init__()
@@ -47,25 +69,14 @@ class Baseline(tf.keras.Model):
         result = inputs[:, :, self.label_index]
         return result[:, :, tf.newaxis]
 
-#single_step_window
-single_step_window = WindowGenerator(
-    input_width=1, label_width=1, shift=1,
-    label_columns=['T (degC)'])
-
-
-# Wide Window Model
-wide_window = WindowGenerator(
-    input_width=24, label_width=24, shift=1,
-    label_columns=['T (degC)'])
-
-
-# Baseline Model
-
 baseline = Baseline(label_index=column_indices['T (degC)'])
 baseline.compile(loss=tf.losses.MeanSquaredError(),
                  metrics=[tf.metrics.MeanAbsoluteError()])
-val_performance['Baseline'] = baseline.evaluate(single_step_window.val, verbose=0)
-performance['Baseline'] = baseline.evaluate(single_step_window.test, verbose=0)
+val_performance['Baseline'] = baseline.evaluate(single_step_window.val, verbose=2)
+performance['Baseline'] = baseline.evaluate(single_step_window.test, verbose=2)
+
+print('Baseline:',val_performance['Baseline'])
+print('Baseline:',performance['Baseline'])
 
 # LSTM Model
 lstm_model = tf.keras.models.Sequential([
@@ -73,8 +84,8 @@ lstm_model = tf.keras.models.Sequential([
     tf.keras.layers.Dense(units=1)
 ])
 history = compile_and_fit(lstm_model, wide_window)
-val_performance['LSTM'] = lstm_model.evaluate(wide_window.val, verbose=0)
-performance['LSTM'] = lstm_model.evaluate(wide_window.test, verbose=0)
+val_performance['LSTM'] = lstm_model.evaluate(wide_window.val, verbose=2)
+performance['LSTM'] = lstm_model.evaluate(wide_window.test, verbose=2)
 
 print('LSTM Val Performance:',val_performance['LSTM'])
 print('LSTM Test Performance:',performance['LSTM'])
@@ -130,7 +141,7 @@ performance['Vanilla_LSTM_NOA'] = NOA.evaluate(wide_window.test, verbose=2)
 
 print('NOA',val_performance['Vanilla_LSTM_NOA'])
 print('NOA',performance['Vanilla_LSTM_NOA'])
-'''
+
 # Peephole Connections
 PeepholeLSTM = tf.keras.models.Sequential([
     tf.keras.layers.RNN(tf.keras.experimental.PeepholeLSTMCell(32), return_sequences=True),
@@ -143,3 +154,27 @@ performance['Vanilla_PeepholeLSTM'] = PeepholeLSTM.evaluate(wide_window.test, ve
 
 print('PeepholeLSTM',val_performance['Vanilla_PeepholeLSTM'])
 print('PeepholeLSTM',performance['Vanilla_PeepholeLSTM'])
+'''
+
+val_results_df = pd.DataFrame.from_dict(val_performance)
+test_results_df = pd.DataFrame.from_dict(performance)
+
+val_results_df.to_csv("val_results_df.cvs")
+test_results_df.to_csv("test_results_df.csv")
+
+x = np.arange(len(performance))
+width = 0.3
+metric_name = 'mean_absolute_error'
+metric_index = lstm_model.metrics_names.index('mean_absolute_error')
+val_mae = [v[metric_index] for v in val_performance.values()]
+test_mae = [v[metric_index] for v in performance.values()]
+
+plt.ylabel('mean_absolute_error [T (degC), normalized]')
+plt.bar(x - 0.17, val_mae, width, label='Validation')
+plt.bar(x + 0.17, test_mae, width, label='Test')
+plt.xticks(ticks=x, labels=performance.keys(),
+           rotation=45)
+_ = plt.legend()
+plt.show()
+
+
